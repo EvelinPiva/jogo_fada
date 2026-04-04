602722a76f605005b7ed651831ffcf40db663325
Evelin <evelin_piva@estudante.sesisenai.org.br>
2026-04-04 03:49:44 -0300

    versão 12

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ let canvas = document.getElementById('des')
 let des = canvas.getContext('2d')
 
 // --- ESTADO DO JOGO ---
-let estadoJogo = 'jogando'
-let selecaoMenu = 1
-let jogando = false
+let estadoJogo = 'menu'
+let menu = false
 
 // --- IMAGENS ---
 let imgMenu = new Image()
@@ -14,6 +13,7 @@ imgMenu.src = './img/TELA_INICIAL.png'
 let musicaFundo = new Audio('./sons/music_fundo.mp3')
 musicaFundo.loop = true
 musicaFundo.volume = 1
+
 
 let somInseto = new Audio('./sons/som_inseto.mp3')
 let somCoracao = new Audio('./sons/coracao_som.mp3')
@@ -41,6 +41,7 @@ let fada2 = new Fada2(100, 225, 90, 90, './img/fada2_2.png')
 document.addEventListener('keydown', teclaPressionada)
 document.addEventListener('keyup', teclaSolta)
 
+
 // --- FUNÇÕES DE CONTROLE ---
 function teclaPressionada(e) {
     const tecla = e.key
@@ -51,7 +52,7 @@ function teclaPressionada(e) {
         musicaFundo.play().catch(err => console.log("Aguardando interação para áudio..."));
     }
 
-    if (estadoJogo === 'jogando') {
+    if (estadoJogo === 'menu') {
         controleFada(tecla, t)
     }
 }
@@ -77,53 +78,75 @@ function controleFada(tecla, t) {
     if (tecla === 'ArrowDown') fada2.dir = 9
     if (tecla === 'ArrowLeft') fada2.dir2 = -11
     if (tecla === 'ArrowRight') fada2.dir2 = 11
-    if (tecla === 'Escape') voltarMenu()
+
 }
 
 function iniciarJogo() {
-    estadoJogo = 'jogando'
-    jogando = true
+    estadoJogo = 'menu'
+    menu = true
     // Resetar o tempo se necessário e dar play
-    musicaFundo.currentTime = 0 
+    musicaFundo.currentTime = 0
     musicaFundo.play().catch(e => console.log('Erro ao tocar música:', e))
 }
-function voltarMenu() {
-    estadoJogo = 'menu'
-    jogando = false
-    musicaInicio.play().catch(e => { })
-    fada.vida = 5
-    fada2.vida = 5
-}
+
 
 // --- LÓGICA ---
 function game_over() {
     if (fada.vida <= 0 || fada2.vida <= 0) {
-        jogando = false
+        menu = false
         estadoJogo = 'gameover'
         musicaFundo.pause()
         musicaFundo.currentTime = 0
     }
 }
+function atualizarFase() {
+    // FADA 1
+    if (fada.pontos >= 90) {
+        fada.fase = 3
+    } else if (fada.pontos >= 40) {
+        fada.fase = 2
+    } else {
+        fada.fase = 1
+    }
+
+    // FADA 2
+    if (fada2.pontos >= 90) {
+        fada2.fase = 3
+    } else if (fada2.pontos >= 40) {
+        fada2.fase = 2
+    } else {
+        fada2.fase = 1
+    }
+}
 
 function atualizaCenario() {
-    if (fada.fase == 2 || fada2.fase == 2) {
+    if (fada.fase === 2 || fada2.fase === 2) {
         canvas.style.backgroundImage = "url('./img/FUNDO_TARDE.png')"
-        if (!abelha1.img.src.includes('/img/pernilongo.png')) {
-            abelha1.img.src = abelha2.img.src = abelha3.img.src = abelha4.img.src = abelha5.img.src = './img/pernilongo.png'
-            abelha1.vel = abelha2.vel = abelha3.vel = abelha4.vel = abelha5.vel = 12
-        }
+
+        abelha1.img.src = './img/pernilongo.png'
+        abelha2.img.src = './img/pernilongo.png'
+        abelha3.img.src = './img/pernilongo.png'
+        abelha4.img.src = './img/pernilongo.png'
+        abelha5.img.src = './img/pernilongo.png'
+
+        abelha1.vel = abelha2.vel = abelha3.vel = abelha4.vel = abelha5.vel = 12
     }
-    if (fada.fase == 3 || fada2.fase == 3) {
+
+    if (fada.fase === 3 || fada2.fase === 3) {
         canvas.style.backgroundImage = "url('./img/FUNDO_NOITE3.png')"
-        if (!abelha1.img.src.includes('/img/insetaoATT.png')) {
-            abelha1.img.src = abelha2.img.src = abelha3.img.src = abelha4.img.src = abelha5.img.src = './img/insetaoATT.png'
-            abelha1.vel = abelha2.vel = abelha3.vel = abelha4.vel = abelha5.vel = 16
-        }
+
+        abelha1.img.src = './img/insetaoATT.png'
+        abelha2.img.src = './img/insetaoATT.png'
+        abelha3.img.src = './img/insetaoATT.png'
+        abelha4.img.src = './img/insetaoATT.png'
+        abelha5.img.src = './img/insetaoATT.png'
+
+        abelha1.vel = abelha2.vel = abelha3.vel = abelha4.vel = abelha5.vel = 16
     }
 }
 
 function colisao() {
-    ;[abelha1, abelha2, abelha3,abelha4,abelha5].forEach(p => {
+    ;[abelha1, abelha2, abelha3, abelha4, abelha5].forEach(p => {
         if (fada.colid(p) || fada2.colid(p)) {
             somInseto.currentTime = 0
             somInseto.play().catch(e => console.log('Erro som inseto:', e))
@@ -131,98 +154,115 @@ function colisao() {
             if (fada2.colid(p)) { p.recomeca(); fada2.vida-- }
         }
     })
-    ;[coracao, coracao2].forEach(c => {
-        if (fada.colid(c)) {
-            somCoracao.currentTime = 0
-            somCoracao.play().catch(e => console.log('Erro som coracao:', e))
-            fada.vida += 1
-            c.recomeca()
-        }
-        if (fada2.colid(c)) {
-            somCoracao.currentTime = 0
-            somCoracao.play().catch(e => console.log('Erro som coracao:', e))
-            fada2.vida += 1
-            c.recomeca()
-        }
-    })
+        ;[coracao, coracao2].forEach(c => {
+            if (fada.colid(c)) {
+                somCoracao.currentTime = 0
+                somCoracao.play().catch(e => console.log('Erro som coracao:', e))
+                fada.vida += 1
+                c.recomeca()
+            }
+            if (fada2.colid(c)) {
+                somCoracao.currentTime = 0
+                somCoracao.play().catch(e => console.log('Erro som coracao:', e))
+                fada2.vida += 1
+                c.recomeca()
+            }
+        })
     if (fada.colid(pocao) || fada2.colid(pocao)) {
         somPocao.currentTime = 0
         somPocao.play().catch(e => console.log('Erro som pocao:', e))
         if (fada.colid(pocao)) fada.pontos += 10
         if (fada2.colid(pocao)) fada2.pontos += 10
         pocao.recomeca()
-       
+
     }
 }
 
 // --- DESENHA ---
 function desenha() {
-    
- if (estadoJogo === 'jogando') {
+
+    if (estadoJogo === 'menu') {
         abelha1.desenhar()
         abelha2.desenhar()
         abelha3.desenhar()
         abelha4.desenhar()
         abelha5.desenhar()
         pocao.desenhar()
-       
+
         coracao.desenhar()
         coracao2.desenhar()
-        
+
         fada.desenhar()
         fada2.desenhar()
-        des.font = '30px JetBrains Mono'
-        des.fillStyle = 'orange'
-        des.fillText('💖: ' + fada.vida, 40, 50)
-        des.fillText('🏅: ' + fada.pontos, 40, 90)
-        console.log(fada.fase)
-        des.fillStyle = 'blue'
-        des.fillText('💖: ' + fada2.vida, 800, 50)
-        des.fillText('🏅: ' + fada2.pontos, 800, 90)
+        // CAIXA FADA 1
+        let y = 30
+        des.fillStyle = "rgba(248, 242, 242, 0.37)"
+        des.fillRect(y - 10, 20, 150, 70)
 
-        if(fada.fase == 1 ){
-            des.fillText('FASE 1: ', 40, 130)
-        }if(fada.fase2 ==2){
-            des.fillText('FASE: 2' + fada.fase, 40, 130)
-        }else{
-            des.fillText('FASE: 3' + fada.fase, 40, 130)
-        }
+        des.strokeStyle = "red"
+        des.strokeRect(y - 10, 20, 150, 70)
+
+        des.font = '20px JetBrains Mono'
+        des.fillStyle = 'red'
+        des.fillText('❤ VIDA: ' + fada.vida, y, 45)
+        des.fillText('★ PONTOS: ' + fada.pontos, y, 65)
+        des.fillText('🎮 FASE: ' + fada.fase, y, 85)
+
+
+        // CAIXA FADA 2
+        let x = 1070
+        des.fillStyle = "rgba(242, 248, 243, 0.37)"
+        des.fillRect(x - 10, 20, 150, 70)
+
+
+        des.strokeStyle = "blue"
+        des.strokeRect(x - 10, 20, 150, 70)
+
+        des.fillStyle = 'blue'
+        des.fillText('❤ VIDA: ' + fada2.vida, x, 45)
+        des.fillText('★ PONTOS: ' + fada2.pontos, x, 65)
+        des.fillText('🎮 FASE: ' + fada2.fase, x, 85)
+        
 
 
     } else if (estadoJogo === 'vitoria') {
         des.fillStyle = 'rgba(0, 50, 0, 0.7)'
         des.fillRect(0, 0, canvas.width, canvas.height)
         des.textAlign = 'center'
-            des.fillStyle = 'gold'
-            des.font = '80px JetBrains Mono'
-            des.fillText('YOU WIN!', canvas.width / 2, 300)
-            des.fillStyle = 'white'
-            des.font = '30px JetBrains Mono'
-            des.fillText('A fada laranja venceu!', canvas.width / 2, 380)
-            des.fillText('Pontuação Final: ' + (fada.pontos + fada2.pontos), canvas.width / 2, 400)
-            des.fillText('Pontuação Fada Azul: ' + (fada2.pontos), canvas.width / 2, 430)
-            des.fillText('Pontuação Fada Laranja: ' + (fada.pontos), canvas.width / 2, 460)
-            des.font = '20px JetBrains Mono'
-            des.fillText('Pressione F5 para jogar novamente', canvas.width / 2, 650)
-            des.textAlign = 'start'
-        }else if (estadoJogo === 'vitoria2') {
-            des.fillStyle = 'rgba(0, 50, 0, 0.7)'
-            des.fillRect(0, 0, canvas.width, canvas.height)
-            des.textAlign = 'center'
-        
-                des.fillStyle = 'gold'
-                des.font = '80px JetBrains Mono'
-                des.fillText('YOU WIN!', canvas.width / 2, 300)
-                des.fillStyle = 'white'
-                des.font = '30px JetBrains Mono'
-                des.fillText('A fada azul venceu!', canvas.width / 2, 380)
-                des.fillText('Pontuação Final: ' + (fada.pontos + fada2.pontos), canvas.width / 2, 400)
-                des.fillText('Pontuação Fada Azul: ' + (fada2.pontos), canvas.width / 2, 430)
-                des.fillText('Pontuação Fada Laranja: ' + (fada.pontos), canvas.width / 2, 460)
-                des.font = '20px JetBrains Mono'
-                des.fillText('Pressione F5 para jogar novamente', canvas.width / 2, 650)
-                des.textAlign = 'start'
-            
+        des.fillStyle = 'gold'
+        des.font = '80px JetBrains Mono'
+        des.fillText('YOU WIN!', canvas.width / 2, 300)
+        des.fillStyle = 'white'
+        des.font = '30px JetBrains Mono'
+        des.fillText('A fada laranja venceu!', canvas.width / 2, 380)
+        des.fillText('Pontuação Final: ' + (fada.pontos + fada2.pontos), canvas.width / 2, 400)
+        des.fillText('Pontuação Fada Azul: ' + (fada2.pontos), canvas.width / 2, 430)
+        des.fillText('Pontuação Fada Laranja: ' + (fada.pontos), canvas.width / 2, 460)
+        des.fillText('Fada Azul ganhou com: ' + (fada2.vida) + ' vidas', canvas.width / 2, 490)
+        des.fillText('Fada Laranja ganhou com: ' + (fada.vida) + ' vidas', canvas.width / 2, 520)
+        des.font = '20px JetBrains Mono'
+        des.fillText('Pressione F5 para jogar novamente', canvas.width / 2, 650)
+        des.textAlign = 'start'
+    } else if (estadoJogo === 'vitoria2') {
+        des.fillStyle = 'rgba(0, 50, 0, 0.7)'
+        des.fillRect(0, 0, canvas.width, canvas.height)
+        des.textAlign = 'center'
+
+        des.fillStyle = 'gold'
+        des.font = '80px JetBrains Mono'
+        des.fillText('YOU WIN!', canvas.width / 2, 300)
+        des.fillStyle = 'white'
+        des.font = '30px JetBrains Mono'
+        des.fillText('A fada azul venceu!', canvas.width / 2, 380)
+        des.fillText('Pontuação Final: ' + (fada.pontos + fada2.pontos), canvas.width / 2, 400)
+        des.fillText('Pontuação Fada Azul: ' + (fada2.pontos), canvas.width / 2, 430)
+        des.fillText('Pontuação Fada Laranja: ' + (fada.pontos), canvas.width / 2, 460)
+        des.fillText('Fada Azul ganhou com: ' + (fada2.vida) + ' vidas', canvas.width / 2, 490)
+        des.fillText('Fada Laranja ganhou com: ' + (fada.vida) + ' vidas', canvas.width / 2, 520)
+        des.font = '20px JetBrains Mono'
+        des.fillText('Pressione F5 para jogar novamente', canvas.width / 2, 650)
+        des.textAlign = 'start'
+
 
     } else if (estadoJogo === 'gameover') {
         des.fillStyle = 'rgba(0, 0, 0, 0.85)'
@@ -236,17 +276,24 @@ function desenha() {
         des.fillText('Pontuação Final: ' + (fada.pontos + fada2.pontos), canvas.width / 2, 320)
         des.fillText('Pontuação Fada Azul: ' + (fada2.pontos), canvas.width / 2, 430)
         des.fillText('Pontuação Fada Laranja: ' + (fada.pontos), canvas.width / 2, 400)
+        
+        des.fillText('Fada Azul perdeu com: ' + (fada2.vida) + ' vidas', canvas.width / 2, 460)
+        des.fillText('Fada Laranja perdeu com: ' + (fada.vida) + ' vidas', canvas.width / 2, 490)
+    
         des.font = '22px JetBrains Mono'
         des.fillStyle = 'gold'
-        des.fillText('Pressione F5 para voltar ao menu', canvas.width / 2, 460)
+        des.fillText('Pressione F5 para voltar ao menu', canvas.width / 2, 570)
         des.textAlign = 'start'
     }
 
 }
+function salvarTempo() {
+    localStorage.setItem('tempoMusica', musica.currentTime);
+}
 
 // --- ATUALIZA ---
 function atualiza() {
-    if (estadoJogo !== 'jogando') return
+    if (estadoJogo !== 'menu') return
     fada.mover()
     fada2.mover()
     atualizaCenario()
@@ -256,15 +303,18 @@ function atualiza() {
     abelha4.mov_obs()
     abelha5.mov_obs()
     pocao.mov_obs()
-  
+
     coracao.mov_obs()
     coracao2.mov_obs()
-    
+
     colisao()
     game_over()
+    atualizarFase()
+    atualizaCenario()
     if (fada.pontos === 130) estadoJogo = 'vitoria'
     if (fada2.pontos === 130) estadoJogo = 'vitoria2'
 }
+
 
 // --- LOOP PRINCIPAL ---
 function main() {
---
+++ readme.md
@@ -0,0 +1,130 @@
+🧚 Fairy Game
+
+1. Identificação do Projeto
+
+Título: Fairy Game
+Desenvolvedor: Evelin Piva
+Professor Orientador: Professor Carlos Roberto da Silva Filho
+Instituição: Sesi Senai
+Curso: Técnico em Desenvolvimento de Sistemas
+
+2. Visão Geral do Sistema
+Descrição
+
+O jogo Fairy Game é uma experiência 2D multiplayer local desenvolvida em JavaScript utilizando a Canvas API. O objetivo principal é controlar duas fadas simultaneamente, coletando itens e desviando de obstáculos enquanto competem em pontuação.
+
+Cada jogador possui uma fada:
+
+🟠 Fada Laranja (WASD)
+🔵 Fada Azul (Setas)
+
+Durante o jogo, surgem obstáculos (insetos), itens coletáveis (corações e poções) e o cenário muda conforme a progressão de fase.
+
+O jogo possui sistema de:
+
+Vidas
+Pontuação
+Progressão de fases
+Colisão
+Sons e música dinâmica
+3. Objetivo
+
+Sobreviver o maior tempo possível, coletar itens e alcançar a maior pontuação.
+
+Vence o jogador que atingir:
+
+130 pontos primeiro
+4. Requisitos Funcionais
+ID	Requisito	Descrição
+RF01	Movimento	Cada fada possui controle independente
+RF02	Sistema de Vidas	Perda de vida ao colidir com insetos
+RF03	Pontuação	Sistema de pontos em tempo real
+RF04	Coletáveis	Corações recuperam vida e poções somam pontos
+RF05	Colisão	Sistema de colisão entre objetos
+RF06	Progressão de Fases	Mudança de fase por pontuação
+RF07	Multijogador	Dois jogadores simultâneos
+RF08	Interface	Menu, Jogo, Vitória e Game Over
+RF09	Áudio	Música e efeitos sonoros dinâmicos
+5. Requisitos Não Funcionais
+ID	Requisito	Descrição
+RNF01	Tecnologia	JavaScript ES6, HTML5 e Canvas
+RNF02	Performance	Atualização via requestAnimationFrame
+RNF03	Portabilidade	Executa diretamente no navegador
+RNF04	Usabilidade	Interface simples e responsiva
+RNF05	Áudio	Controle de música com persistência
+RNF06	Experiência	Feedback visual e sonoro
+6. Regras de Negócio
+ID	Regra
+RN01	Insetos causam perda de vida
+RN02	Corações aumentam a vida
+RN03	Poções aumentam a pontuação
+RN04	Fase 1: até 40 pontos
+RN05	Fase 2: até 90 pontos
+RN06	Fase 3: até 130 pontos
+RN07	Vence quem atingir 130 pontos primeiro
+RN08	Ao perder todas as vidas → Game Over
+7. Controles
+Jogador	Teclas	Ação
+🟠 Fada Laranja	W, A, S, D	Movimentação
+🔵 Fada Azul	↑ ↓ ← →	Movimentação
+8. Mecânicas do Jogo
+🐝 Obstáculos
+Insetos
+Causam perda de vida
+💖 Itens
+Corações: +1 vida
+Poções: +10 pontos
+9. Progressão de Fases
+Fase	Pontuação	Cenário	Dificuldade
+Fase 1	0+	Inicial	Normal
+Fase 2	40+	Tarde	Média
+Fase 3	90+	Noite	Difícil
+10. Sistema de Áudio
+Música de fundo com loop automático
+Sons para:
+Colisão com insetos
+Coleta de coração
+Coleta de poção
+11. Interface do Jogo
+
+O jogo contém:
+
+🎮 Menu inicial
+📜 Instruções
+👩‍💻 Tela do desenvolvedor
+🏆 Tela de vitória
+💀 Tela de Game Over
+12. Lógica Técnica
+
+O jogo utiliza:
+
+canvas para renderização
+Classes para objetos (Fada, Abelha, etc.)
+Loop principal com requestAnimationFrame
+Sistema de estados:
+menu
+gameover
+vitoria
+vitoria2
+13. Estrutura do Projeto
+/img         → imagens do jogo  
+/sons        → áudios  
+/index.html  → página principal  
+/game.js     → lógica do jogo  
+/inicio.html → menu  
+/desenv.html → página do desenvolvedor  
+14. Execução do Projeto
+📥 Abrir o projeto
+
+Abra o arquivo:
+
+index.html
+
+Ou utilize a extensão Live Server no VS Code.
+
+15. Créditos
+
+Desenvolvedora: Evelin Piva
+Instagram: @p.evelinn
+Instituição: Sesi Senai
+Professor: Carlos Roberto da Silva Filho
